88b89159aa9747716f9071c4259e38c8dd8800ca
Glen Mailer <glen@stainlessed.co.uk>
2017-12-03 16:35:10 +0000

    day 3 part 2

--- day3/main.js
+++ day3/main.js
@@ -1,18 +1,38 @@
 const input = Number(process.argv[2] || 10);
-const [grid, result] = distance(input);
-console.log(result);
+const task = process.argv[3] || "A";
+if (task === "A") {
+  const result = buildGrid(
+    input,
+    (n, values, coords) => n,
+    ([x, y]) => Math.abs(x) + Math.abs(y)
+  );
+  console.log(result);
+} else {
+  const result = buildGrid(
+    input,
+    (n, values, coords) =>
+      adjacent(coords)
+        .map(coord => values[coord])
+        .reduce((sum, i) => sum + (i || 0), 0),
+    (coords, values) => values[coords]
+  );
+  console.log(result);
+}
 
-function distance(limit) {
-  let direction = 'R';
-  let x = 0, y = 0;
-  const positions = {[[x, y]]: 1};
+function buildGrid(limit, fillIn, answer) {
+  let direction = "R";
+  let coords = [0, 0];
+  let n = 1;
+  const values = { [coords]: n };
+  let i = 2;
 
-  range(2, limit).forEach((n) => {
-    [[x, y], direction] = advance(positions, [x, y], direction);
-    positions[[x, y]] = n;
-  });
+  while (n < limit) {
+    [coords, direction] = advance(values, coords, direction);
+    values[coords] = n = fillIn(i, values, coords);
+    i += 1;
+  }
 
-  return [positions, Math.abs(x) + Math.abs(y)];
+  return answer(coords, values);
 }
 
 function range(start, end) {
@@ -22,34 +42,37 @@ function range(start, end) {
 function advance(positions, coords, dir) {
   coords = next(coords, dir);
   const changeDir = nextDir(dir);
-  const changeEmpty = (positions[next(coords, changeDir)] == null);
+  const changeEmpty = positions[next(coords, changeDir)] == null;
   return [coords, changeEmpty ? changeDir : dir];
 }
 
 function next([x, y], dir) {
-  switch(dir) {
-    case 'R': return [x + 1, y];
-    case 'L': return [x - 1, y];
-    case 'U': return [x, y - 1];
-    case 'D': return [x, y + 1];
+  switch (dir) {
+    case "R":
+      return [x + 1, y];
+    case "L":
+      return [x - 1, y];
+    case "U":
+      return [x, y - 1];
+    case "D":
+      return [x, y + 1];
   }
 }
 
 function nextDir(dir) {
-  const spiral = ['R', 'U', 'L', 'D'];
+  const spiral = ["R", "U", "L", "D"];
   return spiral[(spiral.indexOf(dir) + 1) % spiral.length];
 }
 
-function extendBounds(bounds, dir, i) {
-  const bound = bounds[dir];
-  switch(dir) {
-    case 'R':
-    case 'D':
-      bound = Math.max(bound, i);
-      break;
-    case 'L':
-    case 'U':
-      bound = Math.min(bound, i);
-  }
-  return Object.assign(bounds, {[dir]: bound});
+function adjacent(coords) {
+  return [
+    next(coords, "R"),
+    next(next(coords, "R"), "U"),
+    next(next(coords, "R"), "D"),
+    next(coords, "L"),
+    next(next(coords, "L"), "U"),
+    next(next(coords, "L"), "D"),
+    next(coords, "U"),
+    next(coords, "D")
+  ];
 }
